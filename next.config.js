474e986227527afa76ddd26c0b73840a9af3f19b
frankly034 <franklynugege@gmail.com>
2023-08-25 15:04:32 +0100

    modified next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  basePath: "/eml-client", // Replace with your GitHub repository name
+  assetPrefix: "/eml-client/",
+  output: "export",
 };
 
 module.exports = nextConfig;
